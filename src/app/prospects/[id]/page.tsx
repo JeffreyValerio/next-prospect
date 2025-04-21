0f5473a18b2062857cf34a09411ce42440cf61c1
JeffreyValerio <jeffreyvalerio@hotmail.com>
2025-04-20 19:40:21 -0600

    mejora en velocidad en la ruta new

--- src/app/prospects/[id]/page.tsx
+++ src/app/prospects/[id]/page.tsx
@@ -1,14 +1,21 @@
 import { redirect } from "next/navigation";
-
 import { getProspectById } from "@/actions/prospects/getProspectsById";
 import { ProspectForm } from "@/components/prospects/ProspectForm";
 
-
 export default async function ProspectPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params
+
+    if (id === 'new') {
+        return <ProspectForm title="Nuevo Prospecto" prospect={{}} />;
+    }
+
     const prospect = await getProspectById(id);
 
     if (!prospect && id !== 'new') redirect('/prospects')
+
+    if (!prospect) {
+        redirect('/prospects');
+    }
 
     const title = id === 'new' ? 'Nuevo Prospecto' : `Editar Prospecto`
     return <ProspectForm title={title} prospect={prospect ?? {}} />
