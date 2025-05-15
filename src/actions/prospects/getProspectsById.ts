d9bd9253c1004246854da13cab88934be30d4af3
JeffreyValerio <jeffreyvalerio@hotmail.com>
2025-05-15 06:51:53 -0600

    mejoras

--- src/actions/prospects/getProspectsById.ts
+++ src/actions/prospects/getProspectsById.ts
@@ -1,11 +1,7 @@
 "use server";
 
 export const getProspectById = async (id: string) => {
-  const res = await fetch(`${process.env.GOOGLE_SCRIPT_URL}?id=${id}`, {
-    next: {
-      revalidate: 60,
-    },
-  });
+  const res = await fetch(`${process.env.GOOGLE_SCRIPT_URL}?id=${id}`);
 
   if (!res.ok) {
     throw new Error("Error fetching prospect by ID");
--- src/app/api/health/route.ts
+++
@@ -1,6 +0,0 @@
-// app/api/health/route.ts
-import { NextResponse } from 'next/server';
-
-export function GET() {
-  return NextResponse.json({ status: 'ok' }, { status: 200 });
-}
