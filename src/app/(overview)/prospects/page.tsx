c0a93a556fdeecd1763acce745844666b57e1234
JeffreyValerio <jeffreyvalerio@hotmail.com>
2025-05-15 07:36:59 -0600

    revalidate

--- src/app/(overview)/prospects/page.tsx
+++ src/app/(overview)/prospects/page.tsx
@@ -1,5 +1,4 @@
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
+export const revalidate = 300; // cachea por 5 minutos
 
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { getProspect } from "@/actions/prospects/get-prospect";
