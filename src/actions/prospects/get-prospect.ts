1882ecae58c88992da64aec6e0f5229fd39ccf74
JeffreyValerio <jeffreyvalerio@hotmail.com>
2025-05-15 12:13:27 -0600

    no cache en prospects

--- src/actions/prospects/get-prospect.ts
+++ src/actions/prospects/get-prospect.ts
@@ -1,11 +1,7 @@
 "use server";
 
 export const getProspect = async () => {
-  const res = await fetch(process.env.GOOGLE_SCRIPT_URL!, {
-    next: {
-      revalidate: 300,
-    },
-  });
+  const res = await fetch(process.env.GOOGLE_SCRIPT_URL!);
 
   if (!res.ok) {
     throw new Error("Error fetching prospects");
