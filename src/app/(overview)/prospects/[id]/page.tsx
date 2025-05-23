3ae4f35e8e570118a5883cfa1ff0644f4675ee52
JeffreyValerio <jeffreyvalerio@hotmail.com>
2025-05-23 16:31:07 -0600

    no generate static

--- src/app/(overview)/prospects/[id]/page.tsx
+++ src/app/(overview)/prospects/[id]/page.tsx
@@ -1,18 +1,9 @@
 import { redirect } from "next/navigation";
 
 import { getClerkUsers } from "@/actions/users/get-clerk-users";
-import { getProspect } from "@/actions/prospects/get-prospect";
 import { getProspectById } from "@/actions/prospects/getProspectsById";
-import { IProspect } from "@/interfaces/prospect.interface";
 import { ProspectForm } from "@/components/prospects/ProspectForm";
 
-export async function generateStaticParams() {
-    const prospects = await getProspect();
-
-    return prospects.map((prospect: IProspect) => ({
-        id: prospect.id
-    }));
-}
 export default async function ProspectPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params
 
@@ -27,5 +18,9 @@ export default async function ProspectPage({ params }: { params: Promise<{ id: s
     if (!prospect && id !== 'new') return redirect('/prospects');
 
     const title = id === 'new' ? 'Nuevo Prospecto' : `Editar Prospecto`
-    return <ProspectForm title={title} prospect={prospect ?? {}} users={users} />
+    return (
+        <section className="relative flex flex-col gap-4">
+            <ProspectForm title={title} prospect={prospect ?? {}} users={users} />
+        </section>
+    )
 }
