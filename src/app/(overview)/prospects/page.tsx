eaa644a812b086b55ca815ab8645886b65ac4e75
JeffreyValerio <jeffreyvalerio@hotmail.com>
2025-05-24 14:16:04 -0600

    mostrando segun usuario

--- src/app/(overview)/prospects/page.tsx
+++ src/app/(overview)/prospects/page.tsx
@@ -1,10 +1,25 @@
 import { getProspects } from "@/actions";
 import { ProspectsTable } from "@/components";
-import { validateUser } from "@/utils/auth";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
 export default async function ProspectsPage() {
-  const prospects = await getProspects();
-  const { isAdmin } = await validateUser();
+
+  const { userId, redirectToSignIn } = await auth();
+  if (!userId) return redirectToSignIn();
+
+  const user = await currentUser()
+  const role = user?.publicMetadata?.role
+
+  const isAdmin = role === "admin";
+
+  const allProspects = await getProspects();
+
+  let prospects = allProspects;
+
+  if (!isAdmin && user?.firstName) {
+    const userName = `${user?.firstName} ${user?.lastName}`;
+    prospects = allProspects.filter((p: { assignedTo: string; }) => p.assignedTo?.trim() === userName);
+  }
 
   return (
     <section className="relative h-full flex flex-col gap-4">
